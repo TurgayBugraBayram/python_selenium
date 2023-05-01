3468bccd5f7cd7833b8710018fb13464cc0b84d0
TurgayBugraBayram <turgaybugrabayram@gmail.com>
2023-05-01 20:20:22 +0300

    Ders2

---
+++ Ders2.py
@@ -0,0 +1,66 @@
+#aritmaik operatörler
+"""
++
+-
+/
+*
+"""
+# print("hello")
+# print(15%4)
+# print(2**4)
+# print(15//4)
+# print(int(15/4))
+
+# x = 5
+# print(x)
+# x = x +3
+# print(x)
+# x =3
+# print(x)
+
+x = 5
+y = 3
+# print(x == y)
+# print(x != y)
+# print(x>y)
+# print(x>=y)
+
+# print(x>=y and x==y)
+# print(x<y or x!=y)
+# print(not(x<=y or x==y))
+
+# diller = "python java c r ruby"
+# print("Java" not in diller)
+#
+# mylist = ["merhaba",1,5.0,True]
+# print(mylist[::2])
+# mylist[0] = "hello"
+# print(mylist)
+# mylist.insert(2,"py")
+# print(mylist)
+# mylist.remove("hello")
+# del mylist
+# print(mylist)
+
+# myTuple = ("merhaba",1,5.0,True)
+# print(type(myTuple))
+# myTupleList = list(myTuple)
+# print(type(myTupleList))
+# myTupleList[0] = "hello"
+# myTuple = tuple(myTupleList)
+# print(myTuple)
+# print(type(myTuple))
+
+# myset = {"merhaba",5,1,True,5}
+# print(myset)
+# myset.
+myDict = {"marka" : "bmw",
+          "yıl": 2005,
+          "yıl": 2004,
+          "model": False,
+          "renk": ["turuncu","krımızı,siyah"]}
+print(myDict)
+print(len(myDict))
+myDict["marka"]= "merco"
+myDict.update({"model": 2.0})
+print(myDict)
