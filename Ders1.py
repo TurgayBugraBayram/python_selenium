218d2f498449f85a3ae5f2ac0b2ab2b66f4f4d36
TurgayBugraBayram <turgaybugrabayram@gmail.com>
2023-05-01 20:20:22 +0300

    Ders1

---
+++ Ders1.py
@@ -0,0 +1,100 @@
+# print("Hello World")
+# print("selenium")
+# merhaba
+"""
+python
+java
+c
+ruby
+"""
+# print('merhaba "hello"')
+# x = 5
+# print(x)
+# y = "hello"
+# print(y)
+# print(type(x))
+# print(type(y))
+# z = 5.0
+# print(z)
+# print(type(z))
+#
+# sayi1 = int(5)
+# sayi2 = str("hello")
+# sayi3 = float(5.0)
+
+# sayi4 = str(5)
+# print(sayi4)
+# print(type(sayi4))
+# sayi5 = int("5")
+# print(sayi5)
+# print(type(sayi5))
+# sayi6 = float("hello")
+# sayi7 = int("hello")
+# print(sayi6)
+# print(sayi7)
+
+# sayi8 = 5
+# print(sayi8)
+# sayi8 = 10
+# print(sayi8)
+#
+# b = 5
+# B = "str"
+# print(B)
+# print(b)
+
+myvar = 1
+my_var = 1
+_my_var = 1
+myVarAbc = 1
+MyVarAbc = 1
+my_var_abc = 1
+MYVAR = 1
+myvar2 = 1
+
+# 2myvar
+# my var =
+# my-var =
+
+
+sayi9, sayi10, sayi11 = 5, "java", 1.0
+# print(sayi9)
+# print(sayi10)
+# print(sayi11)
+# x = y = z = "hello"
+# print(x, y, z)
+# print(f"{x} world {sayi9} merhaba")
+# print("{} world {} merhaba".format(x,sayi9))
+#
+# print(y + sayi10)
+# print(sayi9+sayi11)
+
+a ="""
+merhaba
+bugra
+python
+"""
+b='''
+'''
+print(a)
+
+x = "python hello java"
+# print(x[1])
+# print(x[0])
+# print(x[5])
+# print(x[-1])
+# print(x[-2])
+# print(len(x))
+
+a = "java " in x
+print(a)
+print(type(a))
+abc = True
+dbf = False
+#
+# print(x[2:10:2])
+# print(x[:5:])
+# x.upper()
+# x.lower()
+# print(x.capitalize())
+# print(x.title())
