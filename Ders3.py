d9495671a463098aeaac1706822c2ed440e23335
kesifplus <kesifglobal@gmail.com>
2023-05-19 09:13:55 +0300

    Ders3

---
+++ Ders3.py
@@ -0,0 +1,63 @@
+# a = input()
+# sayi1 = int(input())
+# sayi2 = int(input())
+# if a == "+":
+#     print(sayi1+sayi2)
+# elif a == "-":
+#     print(sayi1-sayi2)
+# elif a == "/":
+#     print(sayi1/sayi2)
+# elif a == "*":
+#     print(sayi1*sayi2)
+# else:
+#     print("hata")
+
+
+# i = 0
+# while i<10:
+#     i += 1
+#     if i == 5:
+#         continue
+#     print(i)
+#     # i = i +1
+#     print("hello")
+# else :
+#     print("i artık ",i)
+
+# for x in "hello":
+#     print(x)
+
+# for i in manav:
+#     if i == "armut":
+#         continue
+#     print(i)
+#
+# for j in range(1,100):
+#     if j%3 == 0 and j%5 == 0:
+#         continue
+#     print(j)
+# else:
+#     print("100 geldi")
+#
+# for x in range(1,100):
+#     if x %5 == 0 and x %3 == 0:
+#         continue
+#     print(x)
+
+# for i in manav:
+#     pass
+# print("abc")
+#
+# while 1:
+#     pass
+# print("abc")
+#
+# manav = ["elma","armut","mandalina"]
+# renk = ["kırmız","mavi","siyah"]
+# for i in manav:
+#     for j in renk:
+#         print(i,j)
+
+aTuple = ("orange","abc","z")
+aTuple.pop(2)
+print(aTuple)
