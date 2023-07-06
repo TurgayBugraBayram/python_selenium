89a8f54b10ec38512c88048ef21f51ad99c3eec7
kesifplus <kesifglobal@gmail.com>
2023-07-06 19:02:19 +0300

    Ders7

---
+++ Ders7_Selenium1.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+#
+# url = "http://github.com"
+# driver.get(url)
+# # driver.maximize_window()
+# print(1,driver.title)
+# driver.save_screenshot("github.png")
+# sleep(2)
+#
+# url = "http://github.com/TurgayBugraBayram"
+# driver.get(url)
+# print(driver.title)
+#
+# if "TurgayBugraBayram" in driver.title:
+#     driver.save_screenshot("profile.png")
+# sleep(2)
+# driver.back()
+# # driver.forward()
+#
+# driver.close()
+
+# url = "http://google.com"
+# driver.get(url)
+# sleep(100)
+
